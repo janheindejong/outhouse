d60bbe0a2ca72c063f25f657edc900a8f1e468a4
wmw123 <wmwaterbolk@gmail.com>
2024-03-17 18:01:31 +0100

    [frontend] added bootstrap navbar

---
+++ outhouse.client/src/components/NavBar/index.jsx
@@ -0,0 +1,22 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavBar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">{"Ons familiehuisje"}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="agenda">Agenda</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export { NavBar };
