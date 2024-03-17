e0a353bbf91def3e15e838a44f0c6356fbdf607e
wmw123 <wmwaterbolk@gmail.com>
2024-03-17 18:09:53 +0100

    [frontend] added navbar routes

--- outhouse.client/src/App.jsx
+++ outhouse.client/src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import { NavBar } from "./components";
+import { Route, Routes } from "react-router-dom";
+import { HomePage, LandingPage } from "./pages";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
