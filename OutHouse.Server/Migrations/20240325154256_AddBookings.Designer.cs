de9f033801185cb40fc018a6c3831237fb296b53
Jan Hein de Jong <jhdejong@vibestechnology.com>
2024-03-25 16:44:25 +0100

    Add migrations for bookings

--- OutHouse.Server/Migrations/20240325154256_AddBookings.Designer.cs
+++ OutHouse.Server/Migrations/20240325154256_AddBookings.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OutHouse.Server.Infrastructure;
 
@@ -11,9 +12,11 @@ using OutHouse.Server.Infrastructure;
 namespace OutHouse.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240325154256_AddBookings")]
+    partial class AddBookings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
